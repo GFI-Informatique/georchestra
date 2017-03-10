c5aecc0e4381d3281f753ba8dd2a10dd51da3de4
gaetan bruel <gaetan.bruel@gfi.fr>
2017-03-10 18:12:53 +0100

    Recreate addon ergo / design with icons and position

--- mapfishapp/src/main/webapp/app/addons/traveler/js/main.js
+++ mapfishapp/src/main/webapp/app/addons/traveler/js/main.js
@@ -12,52 +12,117 @@ GEOR.Addons.traveler = Ext.extend(GEOR.Addons.Base, {
      * record - {Ext.data.record} a record with the addon parameters
      */
     init: function(record) {
-
     	
-    	// create first field set
-    	var dataRadio = function(){
-    		return new Ext.form.CompositeField({
-        		xtype: "compositefield",
-        		fieldLabel:"Rechercher ",
-        		anchor:"100%",
-        		defaults: {
-        			flex:1
-        		},
-        		items:[{
-        			xtype:"radio",
-        			boxLabel:"un lieu",
-        			anchor:"50%",
-        			name:"data"
-        		},{
-        			xtype:"radio",
-        			boxLabel:"une référence",
-        			anchor:"50%",
-        			name:"data"
-        		}]
-    		})
-    	};
+    	var newPoint = function(isStart){
+    		
+        	// create field and buttons
+        	var banField = new Ext.form.CompositeField({
+        			hideLabel: true,
+        			anchor:"100%",
+        			items:[{
+                		xtype:"textfield",
+                		anchor:"70%",
+                		hideLabel:true                			
+                	},{
+                		xtype:"button",                    		
+                		iconCls:"gpsIcon",
+                		cls:"actionBtn"                   	                    			
+                	},{
+                		xtype:"button",
+                		iconCls:"addIcon",
+                		cls:"actionBtn",
+                		hidden: isStart,
+            			handler: function(){
+            				if(Ext.getCmp("hideBtn")){
+            					var btn = Ext.getCmp("hideBtn");
+            					btn.fireEvent("click",btn);
 
+            				}
+            			}
+                	}]
+    		});    		    	
+        	
+        	var comboRef = new Ext.form.ComboBox({
+        		emptyText:"Couches...",
+        		anchor:"70%",
+        		hideLabel:true,
+        		hidden:true
+        	});
+        	
+        	var combAtt = new Ext.form.TextField({
+        		emptyText:"Objets...",
+        		anchor:"70%",
+        		hideLabel:true,
+        		hidden:true
+        	});
+        	
+    		var fSet =  new Ext.form.FieldSet({
+    			anchor:"100%",
+    			border:false,
+    			items:[banField, comboRef, combAtt]
+    		});	    	
+    		
+    		//create checkitem
+        	var checkItem = new Ext.form.Checkbox({
+    			hideLabel:true,
+        		boxLabel: "Référentiels",
+    			listeners:{
+    				"check": function(){
+    					if(this.checked){
+    						banField.hide();
+    						comboRef.show();
+    						combAtt.show();
+    					} else {
+    						banField.show();
+    						comboRef.hide();
+    						combAtt.hide();
+    					}
+    				}
+    			}
+    		});  
+        	
+        	fSet.insert(0, checkItem);
+        	
+    		
+    		return fSet;
+    	}
+    	
+    	var hiddenbtn = new Ext.Button({
+    		hidden:true,
+    		id:"hideBtn",
+    		listeners: {
+    			"click": function(){
+    				if(Ext.getCmp("wayPointPanel")){
+    					var pan = Ext.getCmp("wayPointPanel");
+    					var idx = pan.items && pan.items.length ? pan.items.length - 1 : false;
+    					if(idx){    						    					
+    						pan.insert(idx,newPoint(true));
+    						// now, add cross button to delete step
+    						pan.doLayout();
+    					}
+    				}
+    			}
+    		}
+    	})
 
     	
         // create main window containing free text combo
         this.win = new Ext.Window({
             title: OpenLayers.i18n("traveler.window_title"),
             constrainHeader:true,
-            width: 312,
-            autoHeight:true,
+            autoScroll:true,
+            width: 200,
+            height:200,
             closable: true,
             closeAction: "hide",
             resizable: true,
             collapsible:true,
-            buttons: [{
-                text:"Fermer"        		
-            }],
-            items:[{
+            items:[{ // first window's panel
             	xtype:"panel",
             	items:[{
             		xtype:"compositefield",
             		anchor:"100%",
-            		height: 40,
+            		height: 35,
                 	items:[{
                 		xtype:"button",
                 		height:35,
@@ -110,8 +175,23 @@ GEOR.Addons.traveler = Ext.extend(GEOR.Addons.Base, {
                 				}
                 			}
                 		}
+                	},{
+                		xtype: "spacer",
+                		width:"30"
                 	}]
             	}]
+            },{ // second window's panel
+            	xtype:"panel",
+            	id:"wayPointPanel",
+            	listeners:{
+            		"added": function(panel){
+            			if(!panel.items){
+            				panel.insert(0,newPoint(true));
+                			panel.insert(1,newPoint(false));
+            			}
+            			
+            		}
+            	}
             }]
         });
 
